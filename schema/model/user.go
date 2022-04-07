1c6a3533d48fa2af8935bcd716212446bfbfbdeb
zhaha <80676432+push-and-pop@users.noreply.github.com>
2022-04-07 18:03:32 +0800

    feat

---
+++ schema/model/user.go
@@ -0,0 +1,13 @@
+package model
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Phone string `gorm:"index",size:"10`
+	Name  string
+}
+
+func (u *User) TableName() string {
+	return "User"
+}
