7a31bcfc31b2e6842b156253c42f0bf82ca6c546
zhaha <80676432+push-and-pop@users.noreply.github.com>
2022-04-07 18:44:57 +0800

    dd

--- schema/schema.go
+++ schema/schema.go
@@ -1,1 +1,17 @@
 package schema
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func LinkDb() {
+	var err error
+	dsn := ""
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+}
