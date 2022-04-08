0a6b10484e0534d9d3740e6393ea881fe6eee422
lantian <lan.tian@dbappsecurity.com.cn>
2022-04-08 21:48:59 +0800

    c

--- controller/login/login.go
+++ controller/login/login.go
@@ -18,16 +18,23 @@ func Login(c *gin.Context) {
 	req := &LoginReq{}
 	err := c.ShouldBindJSON(req)
 	if err != nil {
-		c.JSON(401, gin.H{
+		c.JSON(400, gin.H{
 			"err": err,
 		})
+		return
 	}
 	user := model.User{}
 	err = Db.Where("phone = ?", req.Phone).First(&user).Error
 	if errors.Is(err, gorm.ErrRecordNotFound) {
-		Db.Create(&model.User{
+		err = Db.Create(&model.User{
 			Phone: req.Phone,
-		})
+		}).Error
+		if err != nil {
+			c.JSON(400, gin.H{
+				"err": err,
+			})
+			return
+		}
 	}
 	c.JSON(200, gin.H{
 		"code": 200,
