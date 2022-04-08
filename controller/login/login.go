d5e172689d8c74b88fb0643e6d1d0d4c36378889
lantian <lan.tian@dbappsecurity.com.cn>
2022-04-08 21:24:47 +0800

    c

--- controller/login/login.go
+++ controller/login/login.go
@@ -1,12 +1,35 @@
 package login
 
-import "github.com/gin-gonic/gin"
+import (
+	. "car-go/schema"
+	"car-go/schema/model"
+	"errors"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 type LoginReq struct {
-	Phone      string
-	VerifyCode string
+	Phone      string `json:"phone"`
+	VerifyCode string `json:"verify_code"`
 }
 
 func Login(c *gin.Context) {
-
+	req := &LoginReq{}
+	err := c.ShouldBindJSON(req)
+	if err != nil {
+		c.JSON(401, gin.H{
+			"err": err,
+		})
+	}
+	user := model.User{}
+	err = Db.Where("phone = ?", req.Phone).First(&user).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		Db.Create(&model.User{
+			Phone: req.Phone,
+		})
+	}
+	c.JSON(200, gin.H{
+		"code": 200,
+	})
 }
