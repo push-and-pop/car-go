b2484c6e4f0c8c5ccfcc076e864ab5d6fc3880ec
zhaha <80676432+push-and-pop@users.noreply.github.com>
2022-04-08 17:09:45 +0800

    tt

--- router/router.go
+++ router/router.go
@@ -6,21 +6,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type router struct {
-	*gin.Engine
-}
-
 func RegisterRouter(e *gin.Engine) {
-	r := router{
-		e,
-	}
-	group := r.Group("/api/v1")
+	group := e.Group("/api/v1")
 	{
-
+		initLogin(group)
 	}
-	r.initLogin()
 }
 
-func (r *router) initLogin() {
+func initLogin(r *gin.RouterGroup) {
 	r.POST("/login", login.Login)
 }
