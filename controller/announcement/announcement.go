08b92d1b71b816e624b10f4ece8f9d344ac73314
lantian <lan.tian@dbappsecurity.com.cn>
2022-04-10 22:13:47 +0800

    aa

--- controller/announcement/announcement.go
+++ controller/announcement/announcement.go
@@ -2,16 +2,17 @@ package announcement
 
 import (
 	. "car-go/schema"
+	"car-go/schema/model"
 
 	"github.com/gin-gonic/gin"
 )
 
-type Announcement struct {
+type AnnouncementReq struct {
 	Msg string `json:"msg"`
 }
 
 func PubAnnouncement(c *gin.Context) {
-	req := &Announcement{}
+	req := &AnnouncementReq{}
 	err := c.ShouldBindJSON(req)
 	if err != nil {
 		c.JSON(400, gin.H{
@@ -19,5 +20,42 @@ func PubAnnouncement(c *gin.Context) {
 		})
 		return
 	}
-	Db.Model()
+	err = Db.Create(&model.Announce{Msg: req.Msg}).Error
+	if err != nil {
+		c.JSON(400, gin.H{
+			"err": err,
+		})
+		return
+	}
+	c.JSON(200, gin.H{
+		"code": 200,
+	})
+}
+
+type GetAllAnnouncementReq struct {
+	Limit  int32 `json:"limit"`
+	Offset int32 `json:"offset"`
+}
+
+func GetAllAnnouncement(c *gin.Context) {
+	req := &GetAllAnnouncementReq{}
+	err := c.ShouldBindJSON(req)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"err": err,
+		})
+		return
+	}
+	announce := &model.Announce{}
+	err = Db.Limit(int(req.Limit)).Offset(int(req.Offset)).Find(announce).Error
+	if err != nil {
+		c.JSON(400, gin.H{
+			"err": err,
+		})
+		return
+	}
+	c.JSON(200, gin.H{
+		"code": 200,
+		"data": announce,
+	})
 }
--- router/router.go
+++ router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"car-go/controller/announcement"
 	"car-go/controller/login"
 	"car-go/controller/message"
 
@@ -12,13 +13,22 @@ func RegisterRouter(e *gin.Engine) {
 	{
 		initLogin(group)
 		initMessage(group)
+		initAnnounce(group)
 	}
 }
 
+//登录
 func initLogin(r *gin.RouterGroup) {
 	r.POST("/login", login.Login)
 }
 
+//信息
 func initMessage(r *gin.RouterGroup) {
 	r.POST("/checkmessage/creat", message.RegisterMessage)
+}
+
+//公告
+func initAnnounce(r *gin.RouterGroup) {
+	r.POST("/annocement/creat", announcement.PubAnnouncement)
+	r.GET("annocement/get", announcement.GetAllAnnouncement)
 }
