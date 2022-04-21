02c1b81317d2248e7a24308565b3a3f7e3c012fd
zhaha <80676432+push-and-pop@users.noreply.github.com>
2022-04-21 18:10:48 +0800

    dd

--- controller/announcement/announcement.go
+++ controller/announcement/announcement.go
@@ -3,6 +3,7 @@ package announcement
 import (
 	. "car-go/schema"
 	"car-go/schema/model"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -33,29 +34,57 @@ func PubAnnouncement(c *gin.Context) {
 }
 
 type GetAllAnnouncementReq struct {
-	Limit  int32 `json:"limit"`
-	Offset int32 `json:"offset"`
+	Offset int32 `query:"offset"`
+	Limit  int32 `query:"limit"`
+}
+
+type GetAllAnnouncementRsp struct {
+	Announcement []struct {
+		Date int64  `json:"date"`
+		Msg  string `json:"msg"`
+	} `json:"announcement"`
+	Total int64 `json:"total"`
 }
 
 func GetAllAnnouncement(c *gin.Context) {
-	req := &GetAllAnnouncementReq{}
-	err := c.ShouldBindJSON(req)
+	limit, _ := strconv.Atoi(c.Query("limit"))
+	offset, _ := strconv.Atoi(c.Query("offset"))
+	offset = limit * offset
+	if offset < 0 || limit > 5 {
+		c.JSON(400, gin.H{
+			"err": "invalid params",
+		})
+		return
+	}
+	announce := []model.Announce{}
+	err := Db.Offset(int(offset)).Limit(int(limit)).Find(&announce).Error
 	if err != nil {
 		c.JSON(400, gin.H{
 			"err": err,
 		})
 		return
 	}
-	announce := &model.Announce{}
-	err = Db.Limit(int(req.Limit)).Offset(int(req.Offset)).Find(announce).Error
+	var count int64
+	err = Db.Model(&model.Announce{}).Count(&count).Error
 	if err != nil {
 		c.JSON(400, gin.H{
 			"err": err,
 		})
 		return
+	}
+	rsp := GetAllAnnouncementRsp{
+		Announcement: make([]struct {
+			Date int64  `json:"date"`
+			Msg  string `json:"msg"`
+		}, len(announce)),
+		Total: count,
+	}
+	for index, value := range announce {
+		rsp.Announcement[index].Date = value.CreatedAt.Unix()
+		rsp.Announcement[index].Msg = value.Msg
 	}
 	c.JSON(200, gin.H{
 		"code": 200,
-		"data": announce,
+		"data": rsp,
 	})
 }
