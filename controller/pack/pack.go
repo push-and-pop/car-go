8812d3d9856c057de14db39f0a8845e86b1bc4d7
zhaha <80676432+push-and-pop@users.noreply.github.com>
2022-04-24 19:16:08 +0800

    d d

--- controller/pack/pack.go
+++ controller/pack/pack.go
@@ -3,12 +3,28 @@ package pack
 import (
 	. "car-go/schema"
 	"car-go/schema/model"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
-func GetParkList(c *gin.Context) {
+type GetParkListReq struct {
+	Offset int32 `query:"offset"`
+	Limit  int32 `query:"limit"`
+}
 
+func GetParkList(c *gin.Context) {
+	limit, _ := strconv.Atoi(c.Query("limit"))
+	offset, _ := strconv.Atoi(c.Query("offset"))
+	offset = limit * offset
+	if offset < 0 || limit > 5 {
+		c.JSON(400, gin.H{
+			"err": "invalid params",
+		})
+		return
+	}
+	park := []model.CarPark{}
+	err := Db.Offset(int(offset)).Limit(int(limit)).Find(&announce).Error
 }
 
 type CreatCarParkReq struct {
