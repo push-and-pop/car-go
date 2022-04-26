00e40df649e8915cccd0d7bad0bf121ffed2ca31
zhaha <80676432+push-and-pop@users.noreply.github.com>
2022-04-26 19:40:21 +0800

    vv

--- controller/order/order.go
+++ controller/order/order.go
@@ -3,6 +3,7 @@ package order
 import (
 	. "car-go/schema"
 	"car-go/schema/model"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -42,4 +43,43 @@ func RechargeAccount(c *gin.Context) {
 	}
 	tx.Commit()
 	c.JSON(200, gin.H{})
+}
+
+type PayOrderReq struct {
+}
+
+//支付订单
+func PayOrder(c *gin.Context) {
+
+}
+
+type GetOrderListReq struct {
+}
+
+//获取订单列表
+func GetOrderList(c *gin.Context) {
+	limit, _ := strconv.Atoi(c.Query("limit"))
+	offset, _ := strconv.Atoi(c.Query("offset"))
+	offset = limit * offset
+	if offset < 0 || limit > 5 {
+		c.JSON(400, gin.H{
+			"err": "invalid params",
+		})
+		return
+	}
+	user := []model.User{}
+	err := Db.Offset(int(offset)).Limit(int(limit)).Find(&user).Error
+	if err != nil {
+		c.JSON(400, gin.H{
+			"err": err,
+		})
+		return
+	}
+	c.JSON(200, gin.H{
+		"code": 200,
+		"data": gin.H{
+			"total": len(user),
+			"user":  user,
+		},
+	})
 }
--- controller/pack/usePark.go
+++ controller/pack/usePark.go
@@ -1,6 +1,8 @@
 package pack
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 type EnterParkReq struct {
 }
